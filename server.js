5fed7838f7526672ec6c98a586cba968567fdc28
Eli Makston <73192122+EliMakston@users.noreply.github.com>
2023-01-11 04:11:00 +0000

    install morgan, add get and post for envelopes

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const envelopeArray = [];
+let totalBudget;
+
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+app.listen(PORT, () => {
+    console.log(`Now listening on port ${PORT}`);
+})
+
+app.get('/', (req, res, next) => {
+    res.status(200).send('Hello, World')
+})
+
+//TO DO check for valid inputs and make sure no over budget
+app.post('/envelope', (req, res, next) => {
+    const newEnvelope = {
+        id: envelopeArray.length + 1,
+        category: req.params.category,
+        budget: req.params.budget
+    }
+    envelopeArray.push(newEnvelope);
+    res.status(201).send(newEnvelope);
+});
+
+app.get('/envelope', (req, res, next) => {
+    res.status(200).send(envelopeArray);
+})
