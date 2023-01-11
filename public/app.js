30ea5b2e0f5d31cc491299cfa03b441b6f6ea7e5
Eli Makston <73192122+EliMakston@users.noreply.github.com>
2023-01-11 20:42:29 +0000

    still not working

--- public/app.js
+++ public/app.js
@@ -1,1 +1,8 @@
-console.log(`Hello world`);
+function logSubmit(event) {
+    log.textContent = `Form Submitted! Timestamp: ${event.timeStamp}`;
+    event.preventDefault();
+  }
+  
+  const form = document.getElementById('form');
+  const log = document.getElementById('log');
+  form.addEventListener('submit', logSubmit);
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 const PORT = process.env.PORT || 3000;
 
