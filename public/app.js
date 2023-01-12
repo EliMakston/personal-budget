912704dff105016d0cad590201774f5696cf582d
Eli Makston <73192122+EliMakston@users.noreply.github.com>
2023-01-12 05:25:52 +0000

    post envelope through form works

--- public/app.js
+++ public/app.js
@@ -3,8 +3,24 @@ function logSubmit(event) {
     event.preventDefault();
 }
 
-const form = document.querySelector(".form");
+async function sendFormData(event) {
+  event.preventDefault();
+  const formData = Object.fromEntries(new FormData(event.target).entries());
+  const response = await fetch("/envelope", {
+    method: "POST",
+    body: JSON.stringify(formData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.ok) {
+    log.textContent = `Form submitted and envelope made!`;
+  }
+}
+
+const form = document.querySelector("form");
 const log = document.querySelector(".log");
 if (form) {
   form.addEventListener('submit', logSubmit);
+  form.addEventListener('submit', sendFormData);
 }
