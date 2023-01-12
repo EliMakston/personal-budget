c8959d7399587f7cd8e9de6cd742f4f11f0c3b74
Eli Makston <73192122+EliMakston@users.noreply.github.com>
2023-01-12 14:13:06 +0000

    clean up

--- public/index.js
+++ public/index.js
@@ -1,8 +1,3 @@
-function logSubmit(event) {
-    log.textContent = `Form Submitted! Timestamp: ${event.timeStamp}`;
-    event.preventDefault();
-}
-
 async function sendFormData(event) {
   event.preventDefault();
   const formData = Object.fromEntries(new FormData(event.target).entries());
@@ -21,6 +16,5 @@ async function sendFormData(event) {
 const form = document.querySelector("form");
 const log = document.querySelector(".log");
 if (form) {
-  form.addEventListener('submit', logSubmit);
   form.addEventListener('submit', sendFormData);
 }
